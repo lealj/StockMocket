2b677bcf18c9123cd2dd84f34137a25cfa065458
brianhersh <brianhersh@ufl.edu>
2023-02-28 22:05:01 -0500

    removed error messages + added empty input checks

--- frontend/src/app/loginsignuppage/loginsignuppage.component.ts
+++ frontend/src/app/loginsignuppage/loginsignuppage.component.ts
@@ -41,11 +41,14 @@ export class LoginsignuppageComponent implements OnInit {
 
 
   //make a method for ngOnInit
+  //may be unnecessary but keep in case I want to implement this later
   async ngOnInit() {
-    this.LoadAccountInfo()
+    //don't need the following line bc on initialization it shouldn't post anything (only post on button press)
+    //this.LoadAccountInfo()
   }
 
-
+//TA said the GET functionality wouldn't be necessary bc as a frontend, I am just POSTing the info to the backend
+/*
   //this is for getting the info
   async LoadAccountInfo()
   {
@@ -54,40 +57,48 @@ export class LoginsignuppageComponent implements OnInit {
     const valInfo = this.client.get<interfaceAccountInfo[]>('/login');
     this.accountInfo = await lastValueFrom(valInfo);
   }
- 
-
+*/
 
   //this function is called on the log-in button press
   //this is for posting the info
   async AttemptLogin()
   {
-    //(if it doesn't work maybe chnage it to '/rout/login')
-    //Josue said he is implementing this part and to just send it to '/login'
+    console.log("testprint1"); //used to debug and see if the error happens before this log
 
-    const infoToSend = this.client.post('/login', {
+    if(this.usernameForLoginAttempt.length > 0 && this.passwordForLoginAttempt.length > 0) //check if the username or password field are left blank
+    {
+      //Josue said he is implementing this part and to just send it to '/login' (this is the POST call that sends the username and password values to backend)
+      const infoToSend = this.client.post('/login', {
       //push the username and password that is inputted
       usernameForLoginAttempt: this.usernameForLoginAttempt,
       passwordForLoginAttempt: this.passwordForLoginAttempt
-    })
+      })
+      console.log("testprint2"); //used to debug and see if the error happens before this log
 
+      //await lastValueFrom(infoToSend)
 
-    await lastValueFrom(infoToSend)
+      console.log("testprint3"); //used to debug and see if the error happens before this log
 
-
-    // the below code shows that the username and password are properly accepted by the input and stored as a variable for the backend to use in their database management
-    //use this until we can view the values being placed in the server!!
-    console.log("Log In Account Info:");
-    console.log("Username: ", this.usernameForLoginAttempt);
-    console.log("Password: ", this.passwordForLoginAttempt);
-
+      // the below code shows that the username and password are properly accepted by the input and stored as a variable for the backend to use in their database management
+      //use this until we can view the values being placed in the server!!
+      console.log("Log In Account Info:");
+      console.log("Username: ", this.usernameForLoginAttempt);
+      console.log("Password: ", this.passwordForLoginAttempt);
+      //resets the username and password back to '' to clear the input boxes and allow brand new inputs to be entered
+      this.usernameForLoginAttempt = ''
+      this.passwordForLoginAttempt = ''
+    }
+    else if (this.usernameForLoginAttempt.length == 0)
+    {
+      console.log("The username field was left blank...username and password NOT sent to backend");
+    }
+    else if (this.passwordForLoginAttempt.length == 0)
+    {
+      console.log("The password field was left blank...username and password NOT sent to backend");
+    }
 
     //use this to get the info we posted to the server (use await so it doesn't erase the inputted values until after this is called)
-    await this.LoadAccountInfo()
-
-
-    //resets the username and password back to '' to clear the input boxes and allow brand new inputs to be entered
-    this.usernameForLoginAttempt = ''
-    this.passwordForLoginAttempt = ''
+    //await this.LoadAccountInfo()    
   }
 
 
