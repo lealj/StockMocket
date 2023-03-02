c7708968c348b691ecb64b9644bf7aa90ecb2884
FredderickB <92876631+FredderickB@users.noreply.github.com>
2023-03-01 23:08:40 -0500

    added units tests for login component and included back.exe in gitignore

--- frontend/src/app/loginsignuppage/loginsignuppage.component.spec.ts
+++ frontend/src/app/loginsignuppage/loginsignuppage.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
 import { LoginsignuppageComponent } from './loginsignuppage.component';
+import { LoginSignUpService } from './loginsignuppage.service';
 
 describe('LoginsignuppageComponent', () => {
   let component: LoginsignuppageComponent;
@@ -8,7 +10,9 @@ describe('LoginsignuppageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginsignuppageComponent ]
+      imports: [HttpClientTestingModule, FormsModule],
+      declarations: [ LoginsignuppageComponent ],
+      providers: [LoginSignUpService],
     })
     .compileComponents();
 
@@ -20,4 +24,21 @@ describe('LoginsignuppageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize usernameL and passwordL to empty strings', () => {
+    expect(component.usernameL).toEqual('');
+    expect(component.passwordL).toEqual('');
+  });
+
+  it('should reset usernameL and passwordL to empty strings after calling AddOnLogin() method', (async() => {
+    const loginService = TestBed.inject(LoginSignUpService); // create an instance of LoginSignUpService
+    spyOn(loginService, 'AddOnLogin').and.returnValue(Promise.resolve());
+    component.usernameL = 'testuser';
+    component.passwordL = 'testpass';
+    component.AttemptLogin();
+    fixture.whenStable().then(() => {
+      expect(component.usernameL).toEqual('');
+      expect(component.passwordL).toEqual('');
+    });
+  }));
 });
