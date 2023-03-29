831b45a5c0b23a74367fbe98fa13c9aacefab68a
brianhersh <brianhersh@ufl.edu>
2023-03-29 10:47:41 -0400

    added cypress e2e to test correct UN & wrong PW

--- cypress/e2e/LoginPageSpec.cy.js
+++ cypress/e2e/LoginPageSpec.cy.js
@@ -14,18 +14,28 @@
     //enters a username and password to login that does NOT exists in the DB so should fail
     cy.visit('localhost:4200') //visits the local host where the login page info is located
     cy.get('[data-cy="loginUNBox"]').type('asdfghkj') //this should find just the "Enter your username" box
-    cy.get('[data-cy="loginPWBox"]').type('tesasdflkasdhgtFail') //this should find just the "Enter your username" box
+    cy.get('[data-cy="loginPWBox"]').type('tesasdflkasdhgtFail') //this should find just the "Enter your password" box
     cy.get('button').contains('Log In').click() //this presses the button
     //this prints out POST 401 /login meaning that it is correct!!
     //this needs to print out POST 401 to show it doesn't find the account in the backend
     //mention this error in the report and video (will fix it in the next sprint) <--error resolved !!
 
 
+
+    //enters a username that exists and password to login that does NOT exists in the DB so should fail
+    cy.visit('localhost:4200') //visits the local host where the login page info is located
+    cy.get('[data-cy="loginUNBox"]').type('test') //this should find just the "Enter your username" box
+    cy.get('[data-cy="loginPWBox"]').type('tesasdflkasdhgtFail') //this should find just the "Enter your password" box
+    cy.get('button').contains('Log In').click() //this presses the button
+    //this prints out POST 401 /login meaning that it is correct!!
+
+
+
     //enters a username and password to signup that already exists in the DB so should fail
     cy.visit('localhost:4200') //visits the local host where the login page info is located
     //following the cypress-recommended method of finding the components
-    cy.get('[data-cy="signUpUNBox"]').type('test') //this should find just the "Enter your username" box
-    cy.get('[data-cy="signUpPWBox"]').type('test') //this should find just the "Enter your username" box
+    cy.get('[data-cy="signUpUNBox"]').type('test') //this should find just the "Enter a username" box
+    cy.get('[data-cy="signUpPWBox"]').type('test') //this should find just the "Enter a password" box
     cy.get('button').contains('Sign Up').click() //this presses the button
     //this prints out POST 401 /login meaning that it is correct!! <-- returns 401 bc test-test is already in DB
 
@@ -37,7 +47,7 @@
     cy.visit('localhost:4200') //visits the local host where the login page info is located
     //following the cypress-recommended method of finding the components
     cy.get('[data-cy="loginUNBox"]').type('test') //this should find just the "Enter your username" box
-    cy.get('[data-cy="loginPWBox"]').type('test') //this should find just the "Enter your username" box
+    cy.get('[data-cy="loginPWBox"]').type('test') //this should find just the "Enter your password" box
     cy.get('button').contains('Log In').click() //this presses the button
     //this prints out POST 200 /login meaning that it is correct!!
 
@@ -49,8 +59,8 @@
     //enter a username and password to sign up that does NOT already exist in the DB so it should add it
     cy.visit('localhost:4200') //visits the local host where the login page info is located
     //following the cypress-recommended method of finding the components
-    cy.get('[data-cy="signUpUNBox"]').type('testSignUpUN') //this should find just the "Enter your username" box
-    cy.get('[data-cy="signUpPWBox"]').type('testSignUpPW') //this should find just the "Enter your username" box
+    cy.get('[data-cy="signUpUNBox"]').type('testSignUpUN') //this should find just the "Enter a username" box
+    cy.get('[data-cy="signUpPWBox"]').type('testSignUpPW') //this should find just the "Enter a password" box
     cy.get('button').contains('Sign Up').click() //this presses the button
     //this prints out POST 200 /login meaning that it is correct!!
 
@@ -58,8 +68,8 @@
     //enter a username and password to sign up that does already exist in the DB (already added from above test)
     cy.visit('localhost:4200') //visits the local host where the login page info is located
     //following the cypress-recommended method of finding the components
-    cy.get('[data-cy="signUpUNBox"]').type('testSignUpUN') //this should find just the "Enter your username" box
-    cy.get('[data-cy="signUpPWBox"]').type('testSignUpPW') //this should find just the "Enter your username" box
+    cy.get('[data-cy="signUpUNBox"]').type('testSignUpUN') //this should find just the "Enter a username" box
+    cy.get('[data-cy="signUpPWBox"]').type('testSignUpPW') //this should find just the "Enter a password" box
     cy.get('button').contains('Sign Up').click() //this presses the button
     //this prints out POST 401 /login meaning that it is correct!! 
     //the reason it prints 401 versus the above one is bc it is trying to add the un-pw to the database when it was just stored
@@ -73,12 +83,6 @@
     //should return 200 since the username is found in DB and successfully removed!
 
 
-
-
-   
-
-
-   
   })
  })
  
