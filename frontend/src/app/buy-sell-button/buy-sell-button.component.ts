39959adbca5ad3321ac58dc22c0b9a079ca8fe9f
FredderickB <92876631+FredderickB@users.noreply.github.com>
2023-03-29 14:01:32 -0400

    buy-sell buttons are now showing, not functional

--- frontend/src/app/buy-sell-button/buy-sell-button.component.ts
+++ frontend/src/app/buy-sell-button/buy-sell-button.component.ts
@@ -11,26 +11,34 @@ export class BuySellButtonComponent implements OnInit {
   showBuyInput : boolean = false;
   showSellInput : boolean = false;
   quantity: number = 0;
+  username = '';
+
 
   constructor(private shareAction: ShareService){}
   ngOnInit(): void {
-      
+   
   }
 
   onBuyClick(): void {
     //when button is clicked we will show an input box for the quantity of shares
     this.showBuyInput = true;
+    this.showSellInput = false;
   }
 
   Buy(quantity: number){
-    this.shareAction.Buy();
+    username: this.username;
+    this.shareAction.Buy(this.username, "msft", quantity);
+    this.showBuyInput = false; //hides input box after confirming order
   }
 
   onSellClick(){
     this.showSellInput = true;
+    this.showBuyInput = false;
   }
   Sell(quantity: number){
-    this.shareAction.Sell();
+    username: this.username;
+    this.shareAction.Sell(this.username, "msft", quantity);
+    this.showSellInput = false; 
   }
 
 }
--- frontend/src/app/share.service.ts
+++ frontend/src/app/share.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,13 +6,25 @@ import { Injectable } from '@angular/core';
 })
 export class ShareService {
 
-  constructor() { }
+  constructor(private client: HttpClient) { }
 
-  Buy(){
+  Buy(username: string, ticker: string, quantity: number): Promise <any> {
+    const url = `userstock/${username}`;
+    const body = {
+      ticker: ticker,
+      shares: quantity
+    };
 
+    return this.client.post(url, body, {observe: 'response'}).toPromise();
   }
 
-  Sell(){
-    
+  Sell(username: string, ticker: string, quantity: number): Promise <any>{
+    const url = `userstock/${username}`;
+    const body = {
+      ticker: ticker,
+      shares: quantity
+    };
+
+    return this.client.post(url, body, {observe: 'response'}).toPromise();
   }
 }
