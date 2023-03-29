03d4aaa935636f091982a87bcdef90fe05d0fa45
FredderickB <92876631+FredderickB@users.noreply.github.com>
2023-03-29 01:26:49 -0400

    added button component (not functional or visible yet)

---
+++ frontend/src/app/buy-sell-button/buy-sell-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buy-sell-button',
+  templateUrl: './buy-sell-button.component.html',
+  styleUrls: ['./buy-sell-button.component.scss']
+})
+export class BuySellButtonComponent {
+
+}
