b94d81b78c46078f4ac7fa6977de080d88b89ecd
FredderickB <92876631+FredderickB@users.noreply.github.com>
2023-04-02 23:24:21 -0400

    added routing to the  pages planned by this branch

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { MychartComponent } from './mychart/mychart.component';
 import { LoginsignuppageComponent } from './loginsignuppage/loginsignuppage.component';
 import { HeaderComponent } from './header/header.component';
 import { BuySellButtonComponent } from './buy-sell-button/buy-sell-button.component';
+import { HomeComponent } from './home/home.component';
+import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +20,9 @@ import { BuySellButtonComponent } from './buy-sell-button/buy-sell-button.compon
     LoginsignuppageComponent,
     HeaderComponent,
     BuySellButtonComponent,
+    HomeComponent,
+    PortfolioPageComponent,
+    AboutPageComponent,
   ],
   imports: [
     BrowserModule,
